bd41bf7adf68086a9958b19cd42bfffc8f05709d
Guien Joris <guienjoris@gmail.com>
2019-05-09 17:38:39 +0200

    javascript fini

--- script/joris.js
+++ script/joris.js
@@ -1,6 +1,6 @@
 window.onload=function(){
-var btn= document.getElementById("#contact");
-var form= document.getElementById("form_contact");
+    var btn= document.getElementById("#contact");
+    var form= document.getElementById("form_contact");
 btn.onclick = function(){
     if (form.style.display==="none"){
         form.style.display="flex";
