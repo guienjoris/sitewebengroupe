4c40d79595d764edd9aa3a57e324ffe27ae0af6a
21clemence21 <depernetclemence@gmail.com>
2019-05-14 14:47:42 +0200

    coucou

--- script/clemence.js
+++ script/clemence.js
@@ -1,25 +1,17 @@
+var referencesphoto= document.getElementById("referencesphoto");
 
-$(document).ready(function(){
-    $('.toggle').click(function(){
-        $('.toggle').toggleClass('active')
-        $('body').toggleClass('night')
-    })
-});
 
-var referencesphoto = document.getElementById("referencesphoto");
         
-        referencesphoto.addEventListener("click",function(){
-            var imgSrc=referencesphoto.getAttribute("src");
-            if (imgSrc==="images/clientbackground.jpg"){
-                referencesphoto.setAttribute("src","images/client2background.jpg");
-            }
-            else if (imgSrc==="images/client2background.jpg")
-            {
-                referencesphoto.setAttribute("src", "images/client3background.jpg");
-            } else if (imgSrc==="images/client3background.jpg") {
-                referencesphoto.setAttribute("src", "images/client4background.jpg");
-            } else {
-                referencesphoto.setAttribute("src","images/clientbackground.jpg");}
-        })
-
-
+referencesphoto.addEventListener("click",function(){
+    var imgSrc=referencesphoto.getAttribute("src");
+    if (imgSrc==="./images/clientbackground.jpg"){
+        referencesphoto.setAttribute("src","./images/client2background.jpg");
+    }
+    else if (imgSrc==="./images/client2background.jpg")
+    {
+        referencesphoto.setAttribute("src", "./images/client3background.jpg");
+    } else if (imgSrc==="./images/client3background.jpg") {
+        referencesphoto.setAttribute("src", "./images/client4background.jpg");
+    } else {
+        referencesphoto.setAttribute("src","./images/clientbackground.jpg");}
+})
