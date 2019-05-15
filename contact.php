9b472a5c1fd9f799cc60b0dfa799f8300a0f8777
Nabil Hafid Alaoui <nabil.hafidalaoui@gmail.com>
2019-05-15 12:43:43 +0200

    last update

---
+++ contact.php
@@ -0,0 +1,27 @@
+
+<html>
+    <head>
+</head>
+<body>
+<?php 
+
+var_dump("Je m'appelle");die;
+
+$message = "Formulaire de contact: </br>";
+$message .= $_POST['email_contact'];
+$message .= $_POST['nom']'</br>';
+$message .= $_POST['prenom']'</br>';
+$message .= $_POST['demande_contact']'</br>';
+
+
+$to = 'guienjoris@gmail.com';
+$to .= $_POST['email_contact'];
+
+$subject ='Formulaire reçu';
+
+
+mail( $to, $subject, $message);
+?>
+
+
+</body>
